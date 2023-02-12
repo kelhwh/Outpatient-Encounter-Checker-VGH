342d8ac732da75e7b7dd12576146d1dca5bf91eb
kelhwh <frank15871@gmail.com>
2023-02-12 15:09:10 +0800

    correct verification fun

--- verification.py
+++ verification.py
@@ -2,13 +2,15 @@ import time
 
 def verify_length_strict(input, expected_len):
     if len(input) != expected_len:
-        return print("Input length is expected to be {} but get {}!".format(expected_len, len(input)))
+        print("Input length is expected to be {} but get {}!".format(expected_len, len(input)))
+        return True
     else:
-        return
+        return True
 
 def verify_date(input):
     try:
         time.strptime(input, "%Y%m%d")
-        return
+        return True
     except:
-        return print("Input format not recognizable!")
+        print("Input format not recognizable!")
+        return False
